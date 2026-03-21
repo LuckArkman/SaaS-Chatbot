49d4ee1b8531fcd3e1b8c90620e9075a67caf780
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2026-03-21 12:22:49 -0300

    fix: install missing meta-required chromium dependencies for docker and force XVFB in Dockerfile

--- SaaS.OmniChannelPlatform.Services.WhatsAppBot/index.js
+++ SaaS.OmniChannelPlatform.Services.WhatsAppBot/index.js
@@ -60,7 +60,7 @@ async function startBot(sessionId) {
                 }
             },
             {
-                headless: true,
+                headless: false,
                 sessionDataPath: path.join(__dirname, 'tokens'),
                 logQR: false,
                 disableSpins: true,
